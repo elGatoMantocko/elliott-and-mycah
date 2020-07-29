20cc9c3d6437dec895c9ffdfc6ea45caef6f0ca6
Elliott Mantock <emantock@gmail.com>
2020-07-28 17:15:26 -0700

    updated App component

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,22 +1,44 @@
-import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
+import {
+  AppBar,
+  Box,
+  Container,
+  createMuiTheme,
+  Divider,
+  IconButton,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
 import { Menu } from '@material-ui/icons';
 import * as React from 'react';
 import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
 
+const theme = createMuiTheme({
+  spacing: (factor) => `${0.5 * factor}rem`,
+});
+
 export const App = () => (
-  <Router>
-    <Route exact path="/">
-      <Redirect to="/home" />
-    </Route>
-    <Route path="/home">
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu">
-            <Menu />
-          </IconButton>
-          <Typography>Mycah &amp; Elliott</Typography>
-        </Toolbar>
-      </AppBar>
-    </Route>
-  </Router>
+  <ThemeProvider theme={theme}>
+    <Router>
+      <Route exact path="/">
+        <Redirect to="/home" />
+      </Route>
+      <Route path="/home">
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <Menu />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Container>
+          <Box>
+            <Typography variant="h1">Mycah &amp; Elliott</Typography>
+            <Divider />
+            <Typography variant="body1">We are going to have a wedding!</Typography>
+          </Box>
+        </Container>
+      </Route>
+    </Router>
+  </ThemeProvider>
 );
