86b48db2af51426af3362eb10c6bf5ed1e89e090
Elliott Mantock <emantock@gmail.com>
2020-08-05 12:27:08 -0700

    dont use min bundle

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,7 @@
-import * as parseArgs from 'minimist';
 import * as React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import { startServer } from './server/index';
-
-const { development } = parseArgs(process.argv.slice(2));
 
 startServer({
   port: 8080,
@@ -15,7 +12,6 @@ startServer({
         res.render('default', {
           title: 'Mycah &amp; Elliott',
           body: <div id="root"></div>,
-          development,
         });
       },
     },
