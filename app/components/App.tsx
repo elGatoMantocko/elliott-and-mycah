76a1b37789e18410de5a9ad5ffc0cb80b6dacca3
Mycah Davis <mycahddavis93@gmail.com>
2020-08-12 00:13:48 -0700

    ibazel, appbar updates, fab

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,38 +1,53 @@
+import { Box } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import Container from '@material-ui/core/Container';
 import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+import Link from '@material-ui/core/Link';
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Menu from '@material-ui/icons/Menu';
 import * as React from 'react';
 import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
 
 const theme = createMuiTheme({
   spacing: (factor) => `${0.5 * factor}rem`,
 });
-
-export const App = () => (
-  <ThemeProvider theme={theme}>
-    <Router>
-      <Route exact path="/">
-        <Redirect to="/home" />
-      </Route>
-      <Route path="/home">
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start" color="inherit" aria-label="menu">
-              <Menu />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-        <Container maxWidth="md">
-          <Typography variant="h1">Mycah &amp; Elliott</Typography>
-          <Divider />
-          <Typography variant="body1">We are going to have a wedding!</Typography>
-        </Container>
-      </Route>
-    </Router>
-  </ThemeProvider>
-);
+const useStyles = makeStyles({ button: { backgroundColor: 'purple' } });
+export const App = () => {
+  const classes = useStyles();
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/home">
+          <AppBar position="static" color="transparent" elevation={0}>
+            <Toolbar>
+              <Box width="100%" justifyContent="space-evenly" display="flex">
+                {['Wedding Party', 'Venue', 'Itinerary', 'Hotels'].map((linkName, i) => (
+                  <Box key={i} mx="1rem">
+                    <Typography>
+                      <Link href="#" onClick={(e: { preventDefault: () => void }) => e.preventDefault()}>
+                        {linkName}
+                      </Link>
+                    </Typography>
+                  </Box>
+                ))}
+              </Box>
+            </Toolbar>
+          </AppBar>
+          <Container maxWidth="md">
+            <Typography variant="h1">Mycah &amp; Elliott</Typography>
+            <Divider />
+            <Typography variant="body1">We are going to have a wedding!</Typography>
+            <Fab variant="extended" color="primary" className={classes.button}>
+              RSVP
+            </Fab>
+          </Container>
+        </Route>
+      </Router>
+    </ThemeProvider>
+  );
+};
