db2c1bdbe090ba28012628128a4cd4796d5936ee
Elliott Mantock <elGatoMantocko@users.noreply.github.com>
2020-08-15 00:45:52 -0700

    feat: add support for images and move some stuff (#4)

--- app/components/RsvpButton.tsx
+++ app/components/RsvpButton.tsx
@@ -1,14 +1,20 @@
-import Box from '@material-ui/core/Box';
 import Fab, { FabProps } from '@material-ui/core/Fab';
+import { makeStyles } from '@material-ui/core/styles';
 import ReplyIcon from '@material-ui/icons/Reply';
 import * as React from 'react';
 
+const useFabStyles = makeStyles((theme) => ({
+  root: {
+    position: 'sticky',
+    margin: theme.spacing(4),
+    bottom: theme.spacing(4),
+  },
+}));
+
 type RsvpButtonProps = Omit<FabProps, 'children'>;
 export const RsvpButton = (props: RsvpButtonProps) => (
-  <Box position="absolute" bottom="0" right="0" padding="2rem">
-    <Fab variant="extended" color="primary" size="large" {...props}>
-      <ReplyIcon />
-      RSVP
-    </Fab>
-  </Box>
+  <Fab classes={useFabStyles()} variant="extended" color="primary" size="large" {...props}>
+    <ReplyIcon />
+    RSVP
+  </Fab>
 );
