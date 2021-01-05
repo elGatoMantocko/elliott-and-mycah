228c4d3aa308d5ed735ed37e9b8837ec9b839231
Elliott Mantock <emantock@gmail.com>
2021-01-05 00:33:28 -0800

    fix: update map styles

--- app/components/Venue.tsx
+++ app/components/Venue.tsx
@@ -1,14 +1,21 @@
+import { Theme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { GoogleMap } from '@react-google-maps/api';
 import * as React from 'react';
 
 import { ResponsiveContainer } from './ResponsiveContainer';
+
+const useGetMapStyles = (): React.CSSProperties => {
+  const isSmallScreen = useMediaQuery<Theme>((theme) => theme.breakpoints.down('md'));
+  return { width: '100%', height: isSmallScreen ? '75vh' : 400 };
+};
 
 export const Venue = () => (
   <>
     <ResponsiveContainer maxWidth="md">
       <GoogleMap
         zoom={15}
-        mapContainerStyle={{ width: '100%', height: 400 }}
+        mapContainerStyle={useGetMapStyles()}
         center={{
           lat: 47.6453149,
           lng: -122.3085433,
