42e0808566ef2a05c3b9677bc2243fb6908ab82c
Elliott Mantock <emantock@gmail.com>
2021-01-15 03:32:57 -0800

    chore: fix more darkmode compat

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,5 +1,6 @@
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { LoadScript } from '@react-google-maps/api';
 import * as React from 'react';
@@ -11,12 +12,18 @@ import { Menu } from './Menu';
 import { routes } from './Menu/routes';
 import { RsvpButton } from './RsvpButton';
 
+const usePaperStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+});
+
 // TODO: replace the maps api key with a more secure one
 export const App = () => (
   <ParallaxProvider>
     <ThemeProvider theme={useCustomTheme()}>
       <LoadScript googleMapsApiKey="AIzaSyApT_xNp9ePgFYEfdlpw_JJXZG70U1MzXM">
-        <Paper elevation={0} square>
+        <Paper classes={usePaperStyles()} elevation={0} square>
           <Router>
             <Menu />
             <Box position="fixed" bottom="0" left="0" m={4} zIndex={1000}>
--- app/components/Menu/ActiveLink.tsx
+++ app/components/Menu/ActiveLink.tsx
@@ -1,34 +1,35 @@
 import Link, { LinkProps } from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
 import * as React from 'react';
 import { useLocation } from 'react-router';
 
-type Colors =
-  | 'inherit'
-  | 'initial'
-  | 'primary'
-  | 'secondary'
-  | 'textPrimary'
-  | 'textSecondary'
-  | 'error';
+const useLinkColorStyles = makeStyles((theme) => {
+  const { pathname } = useLocation();
+  return {
+    root: {
+      color: ({ disabled, href }: { disabled?: boolean; href: string }) => {
+        const variant = pathname === href ? 'primary' : 'secondary';
+        const mode =
+          theme.palette.type === 'dark'
+            ? 'light'
+            : theme.palette.type === 'light'
+            ? 'dark'
+            : 'main';
+        return disabled ? theme.palette.text.disabled : theme.palette[variant][mode];
+      },
+    },
+  };
+});
+
 type ActiveLinkProps = {
   href: string;
-  activeColor?: Colors;
-  inactiveColor?: Colors;
   disabled?: boolean;
 } & LinkProps;
-export const ActiveLink = ({
-  activeColor = 'primary',
-  inactiveColor = 'secondary',
-  disabled = false,
-  ...linkProps
-}: ActiveLinkProps) => {
-  const { pathname } = useLocation();
-  return (
-    <Link
-      {...linkProps}
-      color={disabled ? 'textSecondary' : pathname === linkProps.href ? activeColor : inactiveColor}
-      underline={disabled ? 'none' : undefined}
-      onClick={(e: React.MouseEvent) => disabled && e.preventDefault()}
-    />
-  );
-};
+export const ActiveLink = ({ disabled = false, ...linkProps }: ActiveLinkProps) => (
+  <Link
+    {...linkProps}
+    classes={useLinkColorStyles({ disabled, href: linkProps.href })}
+    underline={disabled ? 'none' : undefined}
+    onClick={(e: React.MouseEvent) => disabled && e.preventDefault()}
+  />
+);
