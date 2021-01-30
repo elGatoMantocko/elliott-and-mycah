8416e34e9627bd3dec18a2df7452ad30c08c54ee
Elliott Mantock <emantock@gmail.com>
2021-01-29 17:19:19 -0800

    fix: set up chrome in karam run

--- karma.conf.ts
+++ karma.conf.ts
@@ -4,6 +4,10 @@ import { Config } from 'karma';
 
 import { config as webpackConfig } from './webpack.config';
 
+// use puppeteer to find the path to the chrome test runner
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 module.exports = (conf: Config) => {
   delete webpackConfig.entry;
   delete webpackConfig.output;
