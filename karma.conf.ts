cadf19a33aa43810f0627ce4e41826fc43727c28
Elliott Mantock <emantock@gmail.com>
2021-02-04 11:56:38 -0800

    chore: update deps and fix karma config

--- karma.conf.ts
+++ karma.conf.ts
@@ -1,8 +1,23 @@
-import 'karma-webpack';
-
 import { Config } from 'karma';
+import { Configuration } from 'webpack';
 
 import { config as webpackConfig } from './webpack.config';
+
+// HACK(elliott): to fix karma typescript issues the `@types/karma-webpack` module doesn't resolve
+declare module 'karma' {
+  interface WatchOptions {
+    ignored?: string | RegExp | string[];
+  }
+
+  interface KarmaWebpackMiddlewareOptions {
+    watchOptions?: WatchOptions;
+  }
+
+  interface ConfigOptions {
+    webpack: Configuration;
+    webpackMiddleware: KarmaWebpackMiddlewareOptions;
+  }
+}
 
 // use puppeteer to find the path to the chrome test runner
 // eslint-disable-next-line @typescript-eslint/no-var-requires
