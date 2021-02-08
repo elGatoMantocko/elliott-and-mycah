9c09cc932f89841b0e4ba8f2553c8f317c55fdf9
Elliott Mantock <emantock@gmail.com>
2021-02-07 22:55:38 -0800

    feat: adds a basic wedding party page

--- app/components/WeddingParty/PartyMember.tsx
+++ app/components/WeddingParty/PartyMember.tsx
@@ -1,4 +1,14 @@
 import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 import * as React from 'react';
 
-export const PartyMember = <Box></Box>;
+type PartyMemberProps = { name: string; description: string; imgSrc: string; imgAlt?: string };
+export const PartyMember = ({ name, description, imgSrc, imgAlt }: PartyMemberProps) => (
+  <Box m={3}>
+    <img src={imgSrc} alt={imgAlt} style={{ maxWidth: '100%' }} />
+    <Typography variant="h6" component="h1">
+      {name}
+    </Typography>
+    <Typography variant="body2">{description}</Typography>
+  </Box>
+);
--- app/components/WeddingParty/index.tsx
+++ app/components/WeddingParty/index.tsx
@@ -1,17 +1,45 @@
+import Box from '@material-ui/core/Box';
 import * as React from 'react';
 
+import * as austin from '../../images/headshots/austin.webp';
+import * as billbo from '../../images/headshots/billbo.webp';
+import * as brent from '../../images/headshots/brent.webp';
+import * as rob from '../../images/headshots/rob.webp';
 import { ResponsiveContainer } from '../ResponsiveContainer';
 import { ScriptTypography } from '../ScriptTypography';
+import { PartyMember } from './PartyMember';
 
 export const WeddingParty = () => (
   <>
-    <ResponsiveContainer maxWidth="md">
-      <ScriptTypography variant="h2" align="center">
-        Bridemaids
-      </ScriptTypography>
-      <ScriptTypography variant="h2" align="center">
-        Groomsmen
-      </ScriptTypography>
+    <ResponsiveContainer maxWidth="lg">
+      <ScriptTypography variant="h2">Groomsmen</ScriptTypography>
+      <Box display="flex" justifyContent="space-around">
+        <Box width="20vw">
+          <PartyMember
+            name="Billy Shank"
+            description="Billy makes sure to use all of the beans."
+            imgSrc={billbo}
+          />
+        </Box>
+        <Box width="20vw">
+          <PartyMember
+            name="Austin Sims"
+            description="Friend of the groom and a fellow man of of the Trombone."
+            imgSrc={austin}
+          />
+        </Box>
+        <Box width="20vw">
+          <PartyMember name="Rob Mantock" description="Brother of the groom." imgSrc={rob} />
+        </Box>
+        <Box width="20vw">
+          <PartyMember
+            name="Brent Mathis"
+            description="Friend of the groom and really likes beer."
+            imgSrc={brent}
+          />
+        </Box>
+      </Box>
+      <ScriptTypography variant="h2">Bridemaids</ScriptTypography>
     </ResponsiveContainer>
   </>
 );
