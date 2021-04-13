dbd4314752eddd881ab371e7ca06e27d6b90267d
Elliott Mantock <emantock@gmail.com>
2021-04-13 09:41:18 -0700

    chore: update deps, add registry page, and enable links

--- app/components/Registry/index.tsx
+++ app/components/Registry/index.tsx
@@ -1,4 +1,5 @@
 import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 import * as React from 'react';
@@ -7,11 +8,10 @@ import * as amazon from '../../images/logos/amazon.webp';
 import * as crateAndBarrel from '../../images/logos/crate_and_barrel.webp';
 import * as macys from '../../images/logos/macys.webp';
 import * as williamsSanoma from '../../images/logos/williams_sonoma.webp';
-import { ResponsiveContainer } from '../ResponsiveContainer';
 
 export const Registry = () => (
   <>
-    <ResponsiveContainer maxWidth="sm">
+    <Container maxWidth="sm">
       <Box pt={8}>
         <Typography gutterBottom align="center">
           Please know that your presence at our wedding is more than enough! However, for friends
@@ -19,8 +19,8 @@ export const Registry = () => (
           these stores:
         </Typography>
       </Box>
-    </ResponsiveContainer>
-    <ResponsiveContainer maxWidth="md">
+    </Container>
+    <Container maxWidth="md">
       <Box display="flex" justifyContent="space-around" flexWrap="wrap" my={6}>
         <Box maxWidth="11rem" my="auto" py={4} px={1.5}>
           <Link target="_blank" href="https://www.macys.com/wgl/registry/guest/7231659">
@@ -52,6 +52,14 @@ export const Registry = () => (
           </Link>
         </Box>
       </Box>
-    </ResponsiveContainer>
+      <Typography gutterBottom align="center">
+        Items can be shipped to our home address:
+      </Typography>
+      <Typography variant="body2" align="center">
+        12817 14th Ave SW
+        <br />
+        Burien, WA 98146
+      </Typography>
+    </Container>
   </>
 );
