ea2625dd694ee070242b5aded1b2c2a1f9a9233b
Elliott Mantock <emantock@gmail.com>
2021-05-05 09:23:20 -0700

    chore: update deps and add <AddIcon />

--- app/components/Rsvp/RsvpButton.tsx
+++ app/components/Rsvp/RsvpButton.tsx
@@ -1,11 +1,12 @@
 import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 import * as React from 'react';
 
 type RsvpButtonProps = { onClick: (e: React.MouseEvent<HTMLButtonElement>) => void };
 export const RsvpButton = (props: RsvpButtonProps) => {
   return (
     <Fab variant="extended" color="primary" size="large" onClick={props.onClick}>
-      RSVP
+      <AddIcon /> RSVP
     </Fab>
   );
 };
