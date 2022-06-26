8212e10757d2086413d02056a8cc9290fca6a155
Elliott Mantock <emantock@gmail.com>
2022-06-26 14:48:12 -0700

    fix: TZ in tests

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,7 @@ const config: Config.InitialOptions = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
   },
+  globalSetup: '<rootDir>/app/globalSetupJest.ts',
   setupFiles: ['<rootDir>/app/setupJest.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   moduleDirectories: ['node_modules'],
