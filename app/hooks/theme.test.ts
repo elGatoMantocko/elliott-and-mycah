c58e750680dd360ea1948296785f04133e71c800
Elliott Mantock <elliott@reef.ai>
2023-09-27 15:05:07 -0700

    feat: support darkmode 🌚

--- app/hooks/theme.test.ts
+++ app/hooks/theme.test.ts
@@ -7,6 +7,28 @@ it('should match the custom theme', () => {
   expect(result.current).toMatchSnapshot();
 });
 
+it.each(['light', 'dark'] as const)('should render $variant mode', (variant) => {
+  vi.stubGlobal(
+    'matchMedia',
+    // pulled from https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+    vi.fn<Parameters<Window['matchMedia']>, ReturnType<Window['matchMedia']>>((query) => ({
+      matches: variant === 'dark',
+      media: query,
+      onchange: null,
+      addListener: vi.fn(), // deprecated
+      removeListener: vi.fn(), // deprecated
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+      dispatchEvent: vi.fn(),
+    })),
+  );
+
+  const { result } = renderHook(() => useCustomTheme());
+  expect(result.current.palette.mode).eq(variant);
+  expect(window.matchMedia).toHaveBeenCalled();
+  expect(result.current).toMatchSnapshot(variant);
+});
+
 describe('theme.spacing', () => {
   it('should use rems', () => {
     const { result } = renderHook(() => useCustomTheme());
--- app/hooks/theme.ts
+++ app/hooks/theme.ts
@@ -1,38 +1,47 @@
-import { createTheme } from '@mui/material/styles';
-import { useMemo } from 'react';
+import { createTheme, PaletteOptions, useMediaQuery } from '@mui/material';
+import { useCallback, useMemo } from 'react';
 
-export const useCustomTheme = () =>
-  useMemo(
-    () =>
-      createTheme({
-        spacing: (factor: number | string): number | string =>
-          typeof factor === 'number' ? `${0.5 * factor}rem` : factor,
-        palette: {
-          // TODO: play with dark mode but this site is designed for light mode
-          primary: {
-            light: 'rgba(175, 126, 203, 1)',
-            main: 'rgba(139, 77, 174, 1)',
-            dark: 'rgba(114, 78, 135, 1)',
-            contrastText: '#fff',
-          },
-          secondary: {
-            light: 'rgba(104, 177, 104, 1)',
-            main: 'rgba(31, 134, 31, 1)',
-            dark: 'rgba(45, 92, 45, 1)',
-            contrastText: '#fff',
-          },
-          error: {
-            light: '#e57373',
-            main: '#f44336',
-            dark: '#d32f2f',
-            contrastText: '#000',
-          },
-          action: {
-            // default disabled has a lot of opacity and that's stupid
-            disabled: 'rgba(159, 159, 159, 1)',
-            disabledBackground: 'rgba(226, 226, 226, 1)',
-          },
-        },
-      }),
+export const useCustomTheme = () => {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const mode = useMemo(() => (prefersDarkMode ? 'dark' : 'light'), [prefersDarkMode]);
+
+  const spacing = useCallback(
+    (factor: number | string): number | string =>
+      typeof factor === 'number' ? `${0.5 * factor}rem` : factor,
     [],
   );
+
+  const palette = useMemo(
+    (): PaletteOptions | undefined => ({
+      mode,
+      // TODO: play with dark mode but this site is designed for light mode
+      primary: {
+        dark: 'rgba(175, 126, 203, 1)',
+        main: 'rgba(139, 77, 174, 1)',
+        light: 'rgba(114, 78, 135, 1)',
+        contrastText: '#fff',
+      },
+      secondary: {
+        dark: 'rgba(104, 177, 104, 1)',
+        main: 'rgba(31, 134, 31, 1)',
+        light: 'rgba(45, 92, 45, 1)',
+        contrastText: '#fff',
+      },
+      error: {
+        dark: '#e57373',
+        main: '#f44336',
+        light: '#d32f2f',
+        contrastText: '#000',
+      },
+      action: {
+        // default disabled has a lot of opacity and that's stupid
+        disabled: 'rgba(159, 159, 159, 1)',
+        disabledBackground: 'rgba(226, 226, 226, 1)',
+      },
+    }),
+    [mode],
+  );
+
+  return createTheme({ spacing, palette });
+};
