e9f1982ffaccaedc10083055eafc1ea373435f26
Elliott Mantock <elliott@reef.ai>
2023-10-04 15:49:28 -0700

    fix: licenses & bump yarn

---
+++ licenses.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  /**
+   * Function to check validity of a dependencies license.
+   * @param {string} license to check
+   * @returns {boolean} true if the license is valid
+   */
+  isValidLicense(license) {
+    const valid = new RegExp(
+      '\\b(mit|apache\\b.*2|bsd|isc|unlicense|cc0-1.0|python-2.0|cc-by-4.0|cc-by-3.0|wtfpl|0bsd|gpl-3.0+|BlueOak-1.0.0)\\b',
+      'i',
+    );
+    return valid.test(license);
+  },
+  /**
+   * Function to ignore packages - likely because they don't have a valid (or known) license.
+   * @param {string} packageName package name
+   * @returns {boolean} true if the package should be ignored
+   */
+  ignorePackages(packageName) {
+    return /\@esbuild|\@swc/i.test(packageName);
+  },
+};
--- licenses.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  isValidLicense: (license) => {
-    const valid = new RegExp(
-      '\\b(mit|apache\\b.*2|bsd|isc|unlicense|cc0-1.0|python-2.0|cc-by-4.0|cc-by-3.0|wtfpl|0bsd|gpl-3.0+|BlueOak-1.0.0)\\b',
-      'i',
-    );
-    return valid.test(license);
-  },
-};
