09911fbaee290fa9598b1124ba71a5b2114a54be
Elliott Mantock <elliott@reef.ai>
2024-03-30 13:11:26 -0700

    fix: audit licences

--- licenses.config.cjs
+++ licenses.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
    */
   isValidLicense(license) {
     const valid = new RegExp(
-      '\\b(mit|apache\\b.*2|bsd|isc|unlicense|cc0-1.0|python-2.0|cc-by-4.0|cc-by-3.0|wtfpl|0bsd|gpl-3.0+|BlueOak-1.0.0)\\b',
+      '\\b(mit|apache\\b.*2|bsd|isc|unlicense|cc0-1.0|python-2.0|cc-by-4.0|cc-by-3.0|wtfpl|0bsd|gpl-3.0+|BlueOak-1.0.0|MPL-2.0)\\b',
       'i',
     );
     return valid.test(license);
