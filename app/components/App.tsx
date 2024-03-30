692ea2228c6add05926ce7e816ae1b6fa52405c3
Elliott Mantock <elliott@reef.ai>
2024-03-30 12:30:24 -0700

    feat: add speed insights (vercel)

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,6 +1,8 @@
 import { CssBaseline, LinearProgress } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import { LoadScript as LoadGoogleMaps } from '@react-google-maps/api';
+// eslint-disable-next-line import/no-unresolved
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import React from 'react';
 import GithubCorner from 'react-github-corner';
 import {
@@ -18,28 +20,31 @@ import { ScrollToTopOutlet } from './ScrollToTopOutlet';
 import { ServiceWorker } from './ServiceWorker';
 
 export const App = () => (
-  <ParallaxProvider>
-    <ThemeProvider theme={useCustomTheme()}>
-      <CssBaseline />
-      <LoadGoogleMaps googleMapsApiKey="AIzaSyApT_xNp9ePgFYEfdlpw_JJXZG70U1MzXM">
-        <RouterProvider
-          router={createBrowserRouter(
-            createRoutesFromElements(
-              <Route element={<ScrollToTopOutlet />}>
-                <Route element={<MenuOutlet />}>
-                  <Route index element={<Navigate replace to="/us" />} />
-                  <Route path="us" lazy={() => import('../routes/About')} />
-                  <Route path="wedding" lazy={() => import('../routes/Wedding')} />
-                  <Route path="pictures" lazy={() => import('../routes/Pictures')} />
-                </Route>
-              </Route>,
-            ),
-          )}
-          fallbackElement={<LinearProgress />}
-        />
-      </LoadGoogleMaps>
-      <GithubCorner href="https://github.com/elGatoMantocko/elliott-and-mycah" target="_blank" />
-      <ServiceWorker />
-    </ThemeProvider>
-  </ParallaxProvider>
+  <>
+    <ParallaxProvider>
+      <ThemeProvider theme={useCustomTheme()}>
+        <CssBaseline />
+        <LoadGoogleMaps googleMapsApiKey="AIzaSyApT_xNp9ePgFYEfdlpw_JJXZG70U1MzXM">
+          <RouterProvider
+            router={createBrowserRouter(
+              createRoutesFromElements(
+                <Route element={<ScrollToTopOutlet />}>
+                  <Route element={<MenuOutlet />}>
+                    <Route index element={<Navigate replace to="/us" />} />
+                    <Route path="us" lazy={() => import('../routes/About')} />
+                    <Route path="wedding" lazy={() => import('../routes/Wedding')} />
+                    <Route path="pictures" lazy={() => import('../routes/Pictures')} />
+                  </Route>
+                </Route>,
+              ),
+            )}
+            fallbackElement={<LinearProgress />}
+          />
+        </LoadGoogleMaps>
+        <GithubCorner href="https://github.com/elGatoMantocko/elliott-and-mycah" target="_blank" />
+        <ServiceWorker />
+      </ThemeProvider>
+    </ParallaxProvider>
+    <SpeedInsights />
+  </>
 );
