b25140b802e4793132f0f97dd494ab99bc4cb8c3
Elliott Mantock <elliott@reef.ai>
2024-03-30 13:07:15 -0700

    feat: app analytics

--- app/components/AppMetricsOutlet.tsx
+++ app/components/AppMetricsOutlet.tsx
@@ -1,4 +1,6 @@
 // eslint-disable-next-line import/no-unresolved
+import { Analytics } from '@vercel/analytics/react';
+// eslint-disable-next-line import/no-unresolved
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import React from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
@@ -7,6 +9,7 @@ export const AppMetricsOutlet = () => {
   const location = useLocation();
   return (
     <>
+      <Analytics path={location.pathname} />
       <SpeedInsights route={location.pathname} />
       <Outlet />
     </>
